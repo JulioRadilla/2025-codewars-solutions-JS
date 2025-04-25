25d408cf5318d2ee23ad52ba9beb7343fadf7aae
Julio Radilla <itsdigitaljulio@gmail.com>
2025-04-24 20:19:43 -0500

    Adding a new kata solution (forgot once again to save files before pushing)

---
+++ 8-kyu/configure-package-json/solution.js
@@ -0,0 +1,14 @@
+const configuration = {
+    "name": "your-pack-name",
+    "version":"1.0.0",
+    "description": "relearning node",
+    "main": 'index.js',
+    "scripts": {
+    "start": "node index.js"
+    },
+    "author": "Dev Julio",
+    "license": "ISC"
+    
+};
+  // Hint: try creating with `npm init` command
+  // Help! -> https://docs.npmjs.com/files/package.json
